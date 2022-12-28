b00a81af429b9ae198f93b301127151681b5be44
r0man <roman@burningswell.com>
2022-12-28 19:22:50 +0000

    Configure bombaclaat

--- r0man/system/bombaclaat.scm
+++ r0man/system/bombaclaat.scm
@@ -1,51 +1,81 @@
-(use-modules (gnu))
-(use-service-modules networking ssh)
-(use-package-modules screen ssh)
+(use-modules (gnu)
+             (gnu system nss)
+             (nongnu packages linux)
+             (nongnu system linux-initrd))
+
+(use-service-modules desktop networking ssh xorg)
+(use-package-modules certs linux screen ssh)
 
 (define bombaclaat
   (operating-system
+    (kernel linux)
+    (initrd microcode-initrd)
+    (firmware (list sof-firmware linux-firmware))
     (host-name "bombaclaat")
     (timezone "Europe/Berlin")
     (locale "en_US.utf8")
+    (keyboard-layout (keyboard-layout "us"))
 
-    ;; Boot in "legacy" BIOS mode, assuming /dev/sdX is the
-    ;; target hard disk, and "my-root" is the label of the target
-    ;; root file system.
     (bootloader (bootloader-configuration
-                 (bootloader grub-bootloader)
-                 (target "/dev/sdX")))
-    (file-systems (cons (file-system
-                          (device (file-system-label "my-root"))
-                          (mount-point "/")
-                          (type "ext4"))
-                        %base-file-systems))
+                 (bootloader grub-efi-bootloader)
+                 (targets (list "/boot/efi"))
+                 (keyboard-layout keyboard-layout)))
 
-    ;; This is where user accounts are specified.  The "root"
-    ;; account is implicit, and is initially created with the
-    ;; empty password.
-    (users (cons (user-account
-                  (name "alice")
-                  (comment "Bob's sister")
-                  (group "users")
+    (mapped-devices (list (mapped-device
+                           (source (uuid "47e53c2d-0c1b-4d40-b738-8ecba562986d"))
+                           (target "cryptroot")
+                           (type luks-device-mapping))
+                          (mapped-device
+                           (source "bombaclaat")
+                           (targets (list "bombaclaat-home"
+                                          "bombaclaat-root"
+                                          "bombaclaat-swap"))
+                           (type lvm-device-mapping))))
 
-                  ;; Adding the account to the "wheel" group
-                  ;; makes it a sudoer.  Adding it to "audio"
-                  ;; and "video" allows the user to play sound
-                  ;; and access the webcam.
-                  (supplementary-groups '("wheel"
-                                          "audio" "video")))
-                 %base-user-accounts))
+    (file-systems (cons* (file-system
+                           (mount-point "/")
+                           (device (file-system-label "root"))
+                           (type "ext4")
+                           (needed-for-boot? #t)
+                           (dependencies mapped-devices))
+                         (file-system
+                           (mount-point "/boot/efi")
+                           (device (uuid "9B92-14F6" 'fat32))
+                           (type "vfat"))
+                         (file-system
+                           (mount-point "/home")
+                           (device (file-system-label "home"))
+                           (type "ext4")
+                           (needed-for-boot? #t)
+                           (dependencies mapped-devices))
+                         %base-file-systems))
 
-    ;; Globally-installed packages.
-    (packages (cons screen %base-packages))
+    (swap-devices (list (file-system-label "swap")))
 
-    ;; Add services to the baseline: a DHCP client and
-    ;; an SSH server.
-    (services (append (list (service dhcp-client-service-type)
-                            (service openssh-service-type
-                                     (openssh-configuration
-                                      (openssh openssh-sans-x)
-                                      (port-number 2222))))
-                      %base-services))))
+    (users (cons* (user-account
+                   (name "roman")
+                   (comment "Roman Scherer")
+                   (group "users")
+                   (home-directory "/home/roman")
+                   (supplementary-groups '("audio"
+                                           "netdev"
+                                           "video"
+                                           "wheel")))
+                  %base-user-accounts))
+
+    (packages (append (list (specification->package "nss-certs"))
+                      %base-packages))
+
+    (services (modify-services (cons (service gnome-desktop-service-type)
+                                     %desktop-services)
+                (guix-service-type config =>
+                                   (guix-configuration
+                                    (inherit config)
+                                    (substitute-urls
+                                     (append (list "https://substitutes.nonguix.org")
+                                             %default-substitute-urls))
+                                    (authorized-keys
+                                     (append (list (local-file "./keys/nonguix.pub"))
+                                             %default-authorized-guix-keys))))))))
 
 bombaclaat
