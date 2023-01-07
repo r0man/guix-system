14a30e56dc0f8f1945c7747644c9e501ffff7eef
r0man <roman@burningswell.com>
2023-01-07 22:24:44 +0000

    Update uuid, use edge kernel

--- r0man/system/bombaclaat.scm
+++ r0man/system/bombaclaat.scm
@@ -1,57 +1,93 @@
 (define-module (r0man system bombaclaat)
+  #:use-module (asahi initrd)
+  #:use-module (asahi installer)
   #:use-module (asahi packages)
-  #:use-module (gnu)
+  #:use-module (gnu services networking)
+  #:use-module (gnu services ssh)
   #:use-module (gnu system nss)
+  #:use-module (gnu)
   #:use-module (nongnu packages linux)
-  #:use-module (nongnu system linux-initrd))
+  #:use-module (nongnu system linux-initrd)
+  #:use-module (srfi srfi-1))
 
-(use-service-modules desktop networking ssh xorg)
+(use-service-modules networking ssh xorg)
 (use-package-modules certs linux screen ssh)
 
 (define bombaclaat
   (operating-system
-    (kernel asahi-linux)
-    (firmware (list linux-firmware))
     (host-name "bombaclaat")
-    (timezone "Europe/Berlin")
     (locale "en_US.utf8")
+    (timezone "Europe/Berlin")
     (keyboard-layout (keyboard-layout "us"))
-
     (bootloader (bootloader-configuration
                  (bootloader grub-efi-removable-bootloader)
                  (targets (list "/boot/efi"))
                  (keyboard-layout keyboard-layout)))
-
+    (kernel asahi-linux-edge)
+    (kernel-arguments
+     (append '("modprobe.blacklist=radeon"
+               ;; Prevent network interfaces from having really long
+               ;; names. This can cause an issue with wpa_supplicant
+               ;; when you try to connect to a wifi network.
+               "net.ifnames=0"
+               ;; "quiet"
+               )
+             %default-kernel-arguments))
+    (firmware (list linux-firmware asahi-firmware))
+    (initrd asahi-initrd)
     ;; (initrd microcode-initrd)
-    (initrd (lambda (file-systems . rest)
-              ;; Create a standard initrd but set up networking
-              ;; with the parameters QEMU expects by default.
-              (apply base-initrd file-systems
-                     #:qemu-networking? #f
-                     rest)))
-
-    (initrd-modules '(;; "usb-storage"
-                      ;; "uas"
-                      ;; "usbhid"
-                      ;; "hid-apple"
-                      ;; "dm-crypt"
-                      ;; "serpent_generic"
-                      ;; "wp512"
-                      ;; "nls_iso8859-1"
-                      ;; "virtio_pci"
-                      ;; "virtio_balloon"
-                      ;; "virtio_blk"
-                      ;; "virtio_net"
-                      ;; "virtio-rng"
-                      ))
-
-    ;; add the 'net.ifnames' argument to prevent network interfaces
-    ;; from having really long names.  This can cause an issue with
-    ;; wpa_supplicant when you try to connect to a wifi network.
-    (kernel-arguments '("quiet" "modprobe.blacklist=radeon" "net.ifnames=0"))
-
+    ;; (initrd (lambda (file-systems . rest)
+    ;;           ;; Create a standard initrd but set up networking
+    ;;           ;; with the parameters QEMU expects by default.
+    ;;           (apply base-initrd file-systems
+    ;;                  #:qemu-networking? #f
+    ;;                  rest)))
+    (initrd-modules
+     '(;; Asahi
+       ;; For NVMe & SMC
+       ;; "apple-mailbox"
+       ;; For NVMe
+       "nvme-apple"
+       ;; For USB and HID
+       "pinctrl-apple-gpio"
+       ;; SMC core
+       ;; "macsmc" "macsmc-rtkit"
+       ;; For USB
+       "apple-dart"
+       "dwc3"
+       "dwc3-of-simple"
+       ;; "gpio_macsmc"
+       "i2c-apple"
+       "nvmem-apple-efuses"
+       "pcie-apple"
+       "phy-apple-atc"
+       "tps6598x"
+       "xhci-pci"
+       ;; For HID
+       "spi-apple" "spi-hid-apple" "spi-hid-apple-of"
+       ;; For RTC
+       "rtc-macsmc" "simple-mfd-spmi"
+       ;; "spmi-apple-controller"
+       "nvmem_spmi_mfd"
+       ;; For MTP HID
+       "apple-dockchannel" "dockchannel-hid"
+       ;; "apple-rtkit-helper"
+       ;; Guix
+       "usb-storage"
+       "uas"
+       "usbhid"
+       "hid-apple"
+       "dm-crypt"
+       "serpent_generic"
+       "wp512"
+       "nls_iso8859-1"
+       "virtio_pci"
+       "virtio_balloon"
+       "virtio_blk"
+       "virtio_net"
+       "virtio-rng"))
     (mapped-devices (list (mapped-device
-                           (source (uuid "b75f35dd-85fb-4b0d-86ac-9cc0a3dd632c"))
+                           (source (uuid "65d8f2a0-9ebc-44a4-8d68-adeae9221701"))
                            (target "cryptroot")
                            (type luks-device-mapping))
                           (mapped-device
@@ -60,7 +96,6 @@
                                           "bombaclaat-root"
                                           "bombaclaat-swap"))
                            (type lvm-device-mapping))))
-
     (file-systems (cons* (file-system
                            (mount-point "/")
                            (device (file-system-label "root"))
@@ -78,11 +113,9 @@
                            (needed-for-boot? #t)
                            (dependencies mapped-devices))
                          %base-file-systems))
-
     (swap-devices (list (swap-space
                          (target (file-system-label "swap"))
                          (dependencies mapped-devices))))
-
     (users (cons* (user-account
                    (name "roman")
                    (comment "Roman Scherer")
@@ -93,21 +126,25 @@
                                            "video"
                                            "wheel")))
                   %base-user-accounts))
-
-    (packages (append (list (specification->package "nss-certs"))
+    (packages (append (map specification->package
+                           (list "cryptsetup-static"
+                                 "lvm2-static"
+                                 "nss-certs"))
                       %base-packages))
-
-    ;; (services (modify-services (cons (service gnome-desktop-service-type)
-    ;;                                  %desktop-services)
-    ;;             (guix-service-type config =>
-    ;;                                (guix-configuration
-    ;;                                 (inherit config)
-    ;;                                 (substitute-urls
-    ;;                                  (append (list "https://substitutes.nonguix.org")
-    ;;                                          %default-substitute-urls))
-    ;;                                 (authorized-keys
-    ;;                                  (append (list (local-file "./keys/nonguix.pub"))
-    ;;                                          %default-authorized-guix-keys))))))
-    ))
+    (services (modify-services (append (list (service dhcp-client-service-type)
+                                             (service openssh-service-type
+                                                      (openssh-configuration
+                                                       (openssh openssh-sans-x)
+                                                       (port-number 22))))
+                                       %base-services)
+                (guix-service-type config =>
+                                   (guix-configuration
+                                    (inherit config)
+                                    (substitute-urls
+                                     (append (list "https://substitutes.nonguix.org")
+                                             %default-substitute-urls))
+                                    (authorized-keys
+                                     (append (list (local-file "./keys/nonguix.pub"))
+                                             %default-authorized-guix-keys))))))))
 
 bombaclaat
