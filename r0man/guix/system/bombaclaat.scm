9fd78b57cf73c23b9854b07f134073aa5dc0a267
r0man <roman@burningswell.com>
2023-02-04 15:39:32 +0100

    Transform packages

--- r0man/guix/system/bombaclaat.scm
+++ r0man/guix/system/bombaclaat.scm
@@ -2,6 +2,8 @@
   #:use-module (asahi guix initrd)
   #:use-module (asahi guix packages)
   #:use-module (gnu packages certs)
+  #:use-module (gnu packages gl)
+  #:use-module (gnu packages jemalloc)
   #:use-module (gnu packages ssh)
   #:use-module (gnu services avahi)
   #:use-module (gnu services networking)
@@ -11,18 +13,36 @@
   #:use-module (gnu system nss)
   #:use-module (gnu system uuid)
   #:use-module (gnu system)
+  #:use-module (guix packages)
+  #:use-module (guix transformations)
   #:use-module (r0man guix system desktop))
 
 (define %firmware
   (cons* asahi-firmware
          (operating-system-firmware desktop-operating-system)))
 
+;; (define replace-jemalloc-package
+;;   (package-input-rewriting/spec `(("jemalloc" . ,(const jemalloc-16k)))))
+
+;; (define replace-mesa-package
+;;   (package-input-rewriting/spec `(("mesa" . ,(const mesa-asahi-edge)))))
+
+;; (define replace-asahi-packages
+;;   (compose replace-mesa-package replace-jemalloc-package))
+
+;; (replace-asahi-packages asahi-firmware)
+
+(define transform
+  (options->transformation
+   '((with-input . "jemalloc=jemalloc@5.3.0")
+     (with-input . "mesa=mesa-asahi-edge"))))
+
 (define %packages
-  (cons* alsa-ucm-conf-asahi
-         asahi-firmware
-         asahi-scripts
-         mesa-asahi-edge
-         (operating-system-packages desktop-operating-system)))
+  (map transform
+       (cons* alsa-ucm-conf-asahi
+              asahi-firmware
+              asahi-scripts
+              (operating-system-packages desktop-operating-system))))
 
 (define %mapped-devices
   (list (mapped-device
@@ -57,7 +77,7 @@
   (operating-system
     (inherit desktop-operating-system)
     (host-name "bombaclaat")
-    (kernel asahi-linux-edge)
+    (kernel (transform asahi-linux-edge))
     (firmware %firmware)
     (initrd-modules asahi-initrd-modules-edge)
     (mapped-devices %mapped-devices)
