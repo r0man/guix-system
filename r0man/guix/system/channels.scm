33e3881542220aec5bc94fbf4ce92605b4512150
Roman Scherer <roman@burningswell.com>
2023-03-11 16:33:34 +0100

    Update guix commit

--- r0man/guix/system/channels.scm
+++ r0man/guix/system/channels.scm
@@ -6,34 +6,45 @@
          (name 'asahi)
          (url "https://github.com/r0man/asahi-guix.git")
          (branch "main")
-         (commit "e58f8c6135783be798dea9edb4377efc0d30a219")
+         (commit "2b8870cd9f879a29ecc5a6799439335f18b83d12")
          (introduction
           (make-channel-introduction
            "c11f1c583d11b1ed55d34d7041b0c12d51d573e4"
            (openpgp-fingerprint
             "D226 A339 D8DF 4481 5DDE  0CA0 3DDA 5252 7D2A C199"))))
 
+        ;; (channel
+        ;;  (name 'guix)
+        ;;  (url "https://github.com/r0man/guix.git")
+        ;;  (branch "master")
+        ;;  (commit "14dfe8be59ef5cda15540b22cf0cbcb2f6681b29")
+        ;;  (introduction
+        ;;   (make-channel-introduction
+        ;;    "9edb3f66fd807b096b48283debdcddccfea34bad"
+        ;;    (openpgp-fingerprint
+        ;;     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+
         (channel
          (name 'guix)
-         (url "https://github.com/r0man/guix.git")
+         (url "https://git.savannah.gnu.org/git/guix.git")
          (branch "master")
-         (commit "14dfe8be59ef5cda15540b22cf0cbcb2f6681b29")
+         (commit "82fc07363b1600b0bfbf4a3327ea516a7b480f6b")
          (introduction
           (make-channel-introduction
            "9edb3f66fd807b096b48283debdcddccfea34bad"
            (openpgp-fingerprint
             "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
 
-        (channel
-         (name 'nonguix)
-         (url "https://gitlab.com/nonguix/nonguix")
-         (branch "master")
-         (commit "19ef3a8741af9957f372859bab894c3ce0c94f15")
-         (introduction
-          (make-channel-introduction
-           "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-           (openpgp-fingerprint
-            "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+        ;; (channel
+        ;;  (name 'nonguix)
+        ;;  (url "https://gitlab.com/nonguix/nonguix")
+        ;;  (branch "master")
+        ;;  (commit "5a54541e238ed2665c5c3cac99798efe437c5956")
+        ;;  (introduction
+        ;;   (make-channel-introduction
+        ;;    "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+        ;;    (openpgp-fingerprint
+        ;;     "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
 
         (channel
          (name 'r0man-channel)
