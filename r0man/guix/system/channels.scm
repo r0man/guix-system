9f07616c5c1112323886ffc1cbc6a07b01e18a5a
Roman Scherer <roman@burningswell.com>
2023-03-11 12:41:02 +0100

    Add local channel

--- r0man/guix/system/channels.scm
+++ r0man/guix/system/channels.scm
@@ -22,6 +22,11 @@
           (make-channel-introduction
            "9edb3f66fd807b096b48283debdcddccfea34bad"
            (openpgp-fingerprint
-            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))))
+            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+
+        (channel
+         (name 'guix-system)
+         (url "/home/roman/workspace/guix-system")
+         (branch "main"))))
 
 channels
