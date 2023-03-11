36320f752bbc9c51dbdf886fd4582bc6aba0b9d4
Roman Scherer <roman@burningswell.com>
2023-03-11 17:20:08 +0100

    Add back nonguix channel

--- r0man/guix/system/channels.scm
+++ r0man/guix/system/channels.scm
@@ -36,6 +36,17 @@
             "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
 
         (channel
+         (name 'nonguix)
+         (url "https://gitlab.com/nonguix/nonguix")
+         (branch "master")
+         (commit "5a54541e238ed2665c5c3cac99798efe437c5956")
+         (introduction
+          (make-channel-introduction
+           "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+           (openpgp-fingerprint
+            "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+
+        (channel
          (name 'r0man-system)
          (url "git@github.com:r0man/guix-system.git")
          (branch "main")
