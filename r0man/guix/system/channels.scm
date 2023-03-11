bc5b0d9bfee5251b7ba7c70774b966ded3382172
Roman Scherer <roman@burningswell.com>
2023-03-11 16:36:22 +0100

    Use linux-libre 6.2

--- r0man/guix/system/channels.scm
+++ r0man/guix/system/channels.scm
@@ -28,23 +28,23 @@
          (name 'guix)
          (url "https://git.savannah.gnu.org/git/guix.git")
          (branch "master")
-         (commit "82fc07363b1600b0bfbf4a3327ea516a7b480f6b")
+         (commit "e6ed6ab7f3111addda6cda886320ab35e7577e40")
          (introduction
           (make-channel-introduction
            "9edb3f66fd807b096b48283debdcddccfea34bad"
            (openpgp-fingerprint
             "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
 
-        ;; (channel
-        ;;  (name 'nonguix)
-        ;;  (url "https://gitlab.com/nonguix/nonguix")
-        ;;  (branch "master")
-        ;;  (commit "5a54541e238ed2665c5c3cac99798efe437c5956")
-        ;;  (introduction
-        ;;   (make-channel-introduction
-        ;;    "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-        ;;    (openpgp-fingerprint
-        ;;     "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+        (channel
+         (name 'nonguix)
+         (url "https://gitlab.com/nonguix/nonguix")
+         (branch "master")
+         (commit "5a54541e238ed2665c5c3cac99798efe437c5956")
+         (introduction
+          (make-channel-introduction
+           "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+           (openpgp-fingerprint
+            "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
 
         (channel
          (name 'r0man-channel)
