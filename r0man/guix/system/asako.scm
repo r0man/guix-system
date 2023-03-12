3382c145be3612bfa8198a2a67cb21f0c70d5879
Roman Scherer <roman@burningswell.com>
2023-03-12 18:53:31 +0000

    Add Asako system

--- r0man/guix/system/asako.scm
+++ r0man/guix/system/asako.scm
@@ -1,4 +1,4 @@
-(define-module (r0man guix system bombaclaat)
+(define-module (r0man guix system asako)
   #:use-module (asahi guix initrd)
   #:use-module (asahi guix packages)
   #:use-module (asahi guix services)
@@ -42,15 +42,15 @@
          (target "cryptroot")
          (type luks-device-mapping))
         (mapped-device
-         (source "bombaclaat")
-         (targets (list "bombaclaat-root"
-                        "bombaclaat-swap"))
+         (source "asako")
+         (targets (list "asako-root"
+                        "asako-swap"))
          (type lvm-device-mapping))))
 
 (define %file-systems
   (cons* (file-system
            (mount-point "/")
-           (device "/dev/mapper/bombaclaat-root")
+           (device "/dev/mapper/asako-root")
            (type "ext4")
            (needed-for-boot? #t)
            (dependencies %mapped-devices))
@@ -87,13 +87,13 @@
 
 (define %swap-devices
   (list (swap-space
-         (target "/dev/mapper/bombaclaat-swap")
+         (target "/dev/mapper/asako-swap")
          (dependencies %mapped-devices))))
 
-(define bombaclaat
+(define asako
   (operating-system
     (inherit desktop-operating-system)
-    (host-name "bombaclaat")
+    (host-name "asako")
     (kernel (replace-asahi asahi-linux-edge))
     (firmware %firmware)
     (initrd-modules asahi-initrd-modules-edge)
@@ -103,4 +103,4 @@
     (services %services)
     (swap-devices %swap-devices)))
 
-bombaclaat
+asako
