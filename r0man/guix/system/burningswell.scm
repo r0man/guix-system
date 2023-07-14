2f70a32411adfca50fc1433f5a6636a07e15d656
Roman Scherer <roman@burningswell.com>
2023-07-14 10:04:04 +0200

    Fix unattended upgrade

--- r0man/guix/system/burningswell.scm
+++ r0man/guix/system/burningswell.scm
@@ -22,6 +22,7 @@
   #:use-module (gnu system nss)
   #:use-module (gnu system uuid)
   #:use-module (gnu system)
+  #:use-module (guix channels)
   #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module (r0man guix system base)
@@ -141,35 +142,11 @@
 (define %unattended-upgrade-service
   (service unattended-upgrade-service-type
            (unattended-upgrade-configuration
-            (channels #~(list (channel
-                               (name 'asahi)
-                               (url "https://github.com/r0man/asahi-guix.git")
-                               (branch "main")
-                               (introduction
-                                (make-channel-introduction
-                                 "c11f1c583d11b1ed55d34d7041b0c12d51d573e4"
-                                 (openpgp-fingerprint
-                                  "D226 A339 D8DF 4481 5DDE  0CA0 3DDA 5252 7D2A C199"))))
-
+            (channels #~(list asahi-channel
+                              guix-channel
                               (channel
-                               (name 'guix)
-                               (url "https://git.savannah.gnu.org/git/guix.git")
-                               (branch "master")
-                               (introduction
-                                (make-channel-introduction
-                                 "9edb3f66fd807b096b48283debdcddccfea34bad"
-                                 (openpgp-fingerprint
-                                  "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-
-                              (channel
-                               (name 'r0man-system)
-                               (url "/root/guix-system")
-                               (branch "main")
-                               (introduction
-                                (make-channel-introduction
-                                 "754146ab5979be91a3ed69c99b9dbccb4d06b6bd"
-                                 (openpgp-fingerprint
-                                  "D226 A339 D8DF 4481 5DDE  0CA0 3DDA 5252 7D2A C199")))))))))
+                               (inherit system-channel)
+                               (url "/root/guix-system")))))))
 
 (define %mapped-devices
   (list (mapped-device
