40d5532a7eaebc94b63491eb0a43920cabf28056
Roman Scherer <roman@burningswell.com>
2023-07-15 17:18:36 +0200

    Inline channels

--- r0man/guix/system/burningswell.scm
+++ r0man/guix/system/burningswell.scm
@@ -143,13 +143,33 @@
 (define %unattended-upgrade-service
   (service unattended-upgrade-service-type
            (unattended-upgrade-configuration
-            (channels (with-imported-modules (source-module-closure
-                                              '((r0man guix system channels)))
-                        #~(list asahi-channel
-                                guix-channel
-                                (channel
-                                 (inherit system-channel)
-                                 (url "/root/guix-system")))))
+            (channels #~(list (channel
+                               (name 'asahi)
+                               (url "https://github.com/r0man/asahi-guix.git")
+                               (branch "main")
+                               (introduction
+                                (make-channel-introduction
+                                 "c11f1c583d11b1ed55d34d7041b0c12d51d573e4"
+                                 (openpgp-fingerprint
+                                  "D226 A339 D8DF 4481 5DDE  0CA0 3DDA 5252 7D2A C199"))))
+                              (channel
+                               (name 'guix)
+                               (url "https://git.savannah.gnu.org/git/guix.git")
+                               (branch "master")
+                               (introduction
+                                (make-channel-introduction
+                                 "9edb3f66fd807b096b48283debdcddccfea34bad"
+                                 (openpgp-fingerprint
+                                  "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+                              (channel
+                               (name 'r0man-system)
+                               (url "/root/guix-system")
+                               (branch "main")
+                               (introduction
+                                (make-channel-introduction
+                                 "754146ab5979be91a3ed69c99b9dbccb4d06b6bd"
+                                 (openpgp-fingerprint
+                                  "D226 A339 D8DF 4481 5DDE  0CA0 3DDA 5252 7D2A C199"))))))
             (operating-system-file
              (file-append (local-file "../../.." "config-dir" #:recursive? #t)
                           "/r0man/guix/system/burningswell.scm"))
