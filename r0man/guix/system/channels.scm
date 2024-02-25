3ce30980b05e003c476feaa26c6d11fd832b8836
Roman Scherer <roman@burningswell.com>
2024-02-25 19:16:21 +0100

    Update channels

--- r0man/guix/system/channels.scm
+++ r0man/guix/system/channels.scm
@@ -4,29 +4,29 @@
 (define-public asahi-channel
   (channel
    (name 'asahi)
-   (url "https://github.com/r0man/asahi-guix.git")
    (branch "main")
+   (url "https://github.com/asahi-guix/channel.git")
    (introduction
     (make-channel-introduction
-     "c11f1c583d11b1ed55d34d7041b0c12d51d573e4"
+     "3eeb493b037bea44f225c4314c5556aa25aff36c"
      (openpgp-fingerprint
       "D226 A339 D8DF 4481 5DDE  0CA0 3DDA 5252 7D2A C199")))))
 
-(define-public guix-channel
+(define guix-channel
   (channel
    (name 'guix)
-   (url "https://git.savannah.gnu.org/git/guix.git")
-   (branch "master")
+   (url "https://github.com/asahi-guix/guix.git")
+   (branch "main")
    (introduction
     (make-channel-introduction
-     "9edb3f66fd807b096b48283debdcddccfea34bad"
+     "b8b7760de185968c54240b31c0ff186652acf7a0"
      (openpgp-fingerprint
-      "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+      "D226 A339 D8DF 4481 5DDE  0CA0 3DDA 5252 7D2A C199")))))
 
 (define-public system-channel
   (channel
    (name 'r0man-system)
-   (url "git@github.com:r0man/guix-system.git")
+   (url "https://github.com/r0man/guix-system")
    (branch "main")
    (introduction
     (make-channel-introduction
