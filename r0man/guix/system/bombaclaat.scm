6b3587f43a2ecac7a61e6ac9b7b66a4d030e8571
Roman Scherer <roman@burningswell.com>
2024-04-05 15:12:12 +0200

    Tweak sound setup

--- r0man/guix/system/bombaclaat.scm
+++ r0man/guix/system/bombaclaat.scm
@@ -70,22 +70,21 @@
            (type "vfat"))
          %base-file-systems))
 
-(define %speakersafetyd-service
-  (service speakersafetyd-service-type))
-
 (define %services
-  (modify-services (cons* (service alsa-service-type)
+  (modify-services (cons* (service asahi-firmware-service-type)
+                          (service alsa-service-type)
                           (service kernel-module-loader-service-type '("asahi" "appledrm"))
+                          (service pipewire-service-type)
+                          (service speakersafetyd-service-type)
                           (simple-service 'asahi-config etc-service-type
                                           (list `("modprobe.d/asahi.conf"
                                                   ,(plain-file "asahi.conf" "options asahi debug_flags=0"))))
                           %qemu-service-aarch64
-                          %speakersafetyd-service
                           %udev-backlight-service
                           %udev-kbd-backlight-service
-                          (append (operating-system-user-services desktop-operating-system)
-                                  (list (service asahi-firmware-service-type))))
+                          (operating-system-user-services desktop-operating-system))
     (delete sound:alsa-service-type)
+    (delete sound:pulseaudio-service-type)
     (slim-service-type config =>
                        (slim-configuration
                         (inherit config)
