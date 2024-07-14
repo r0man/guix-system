9e9761077b83ac1b8d44aeddc5b8ce8d2357e4eb
Roman Scherer <roman@burningswell.com>
2024-07-14 16:07:10 +0200

    Update channel introduction

--- r0man/guix/system/channels.scm
+++ r0man/guix/system/channels.scm
@@ -19,7 +19,7 @@
    (branch "main")
    (introduction
     (make-channel-introduction
-     "2a930ea8007f81f468b4b6cb46c2bcd55e33b2a5"
+     "47ad87a9190dd8710121fd24b0e9169dd828bfa8"
      (openpgp-fingerprint
       "D226 A339 D8DF 4481 5DDE  0CA0 3DDA 5252 7D2A C199")))))
 
